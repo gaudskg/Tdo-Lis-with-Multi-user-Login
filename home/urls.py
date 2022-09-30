d29b21938dfc813ab5ee558dff6d83335fa56603
Sachin <gaudsachin43@gmail.com>
2022-10-01 03:34:20 +0530

    login logout and register forms added

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('sign-in/', views.signin, name='sign_in'),
+    path('sign-up/', views.signup, name='sign_up'),
+    path('sign-out/',views.signout, name='sign_out')
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 def home(request):
-    return HttpResponse("HOME PAGE")
+    if request.method == 'POST':
+        email = request.POST['user_email']
+        password = request.POST['user_password']
+        print(email,password,"***********************************")
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            context = { 'error_msg': "Please enter the correct username or password" }
+            return redirect('/sign-in',context)
+    return render(request,'home.html')
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        return render(request,'register.html')
+
+def signin(request):
+    if request.method == 'POST':
+        email = request.POST['user_email']
+        password = request.POST['user_password']
+        print(email,password,"***********************************")
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            context = { 'error_msg': "Please enter the correct username or password" }
+            return redirect('/sign-in',context)
+    return render(request,'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        name = request.POST['user_name']
+        email = request.POST['user_email']
+        password = request.POST['user_password']
+        confirm_password = request.POST['user_confirm_password']
+        print(name,email,password,confirm_password,"******************************")
+    # return render(request,'login.html')
+    return HttpResponse("SIGNUP PAGE")
+
+def signout(request):
+    logout(request)
+    return redirect('/sign-in')
